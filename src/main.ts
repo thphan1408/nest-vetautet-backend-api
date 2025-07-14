b590870b636f7fe2fced3a175134fb3f1b3332db
Thành Phan <th.phan1408@gmail.com>
2025-07-14 09:47:28 +0700

    config: swagger docs api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Vé tàu tết - API')
+    .setDescription('API documentation for Vé tàu tết')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // Keep token after page refresh
+    },
+  });
+
+  // Enable cors
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  });
+
+  await app.listen(process.env.PORT || 8080);
+  console.log('Server is running on port:', process.env.PORT || 8080);
+  console.log(
+    'Swagger docs available at: http://localhost:' +
+      (process.env.PORT || 8080) +
+      '/api/docs',
+  );
 }
 bootstrap();
